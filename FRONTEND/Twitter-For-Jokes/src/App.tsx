e2f249ceba1b0fca330955ef599185ead579466d
gest2356 <vojta.javurek@gmail.com>
2026-01-13 12:34:30 +0100

    add: frontend whit basic comunication whit users controller

---
+++ FRONTEND/Twitter-For-Jokes/src/App.tsx
@@ -0,0 +1,8 @@
+import DataTable from "./Modules/Table.tsx";
+
+function App() {
+  return <DataTable></DataTable>
+
+}
+
+export default App
---
+++ FRONTEND/Twitter-For-Jokes/src/Modules/Table.tsx
@@ -0,0 +1,45 @@
+﻿import React, { useState, useEffect } from 'react';
+
+
+interface Item {
+    usrId: number;
+    userName: string;
+    password: string;
+}
+
+const DataTable: React.FC = () => {
+    // 2. Říkáme, že state bude pole objektů typu Item
+    const [data, setData] = useState<Item[]>([]);
+
+    useEffect(() => {
+        fetch('http://localhost:65451/api/Users')
+            .then(response => response.json())
+            // TypeScript teď ví, že 'vysledek' je typu Item[]
+            .then((resalt: Item[]) => setData(resalt));
+    }, []);
+
+    console.table(data);
+    return (
+        <table border={1}>
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Název</th>
+                <th>password</th>
+            </tr>
+            </thead>
+            <tbody>
+            {data.map((item) => (
+                <tr key={item.usrId}>
+                    <td>{item.usrId}</td>
+                    <td>{item.userName}</td>
+                    <td>{item.password}</td>
+                </tr>
+            ))}
+            </tbody>
+        </table>
+
+    );
+};
+
+export default DataTable;
