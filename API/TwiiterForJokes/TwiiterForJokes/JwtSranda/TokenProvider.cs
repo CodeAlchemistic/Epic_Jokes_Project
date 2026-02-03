ccd305f881c6b5835c33b40f10e64d2c71fc7e24
TrueTexar <SobotkaTomas@sssvt.cz>
2026-02-03 19:26:35 +0100

    update: JWT tokenProvider

---
+++ API/TwiiterForJokes/TwiiterForJokes/JwtSranda/TokenProvider.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using TwiiterForJokes.Entitys;
+using JwtClaimNames = System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames;
+
+namespace TwiiterForJokes.JwtSranda
+{
+    internal sealed class TokenProvider(IConfiguration configuration)
+    {
+
+        public string Create(Usr user)
+        {
+            string secretKey = configuration["Jwt:Secret"]!;
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity([
+
+                  
+                    new Claim(JwtClaimNames.Sub, user.UsrId.ToString()),
+                    new Claim(JwtClaimNames.Sub, user.UserName.ToString())
+                    
+                ]),
+
+                Expires = DateTime.UtcNow.AddMinutes(configuration.GetValue<int>("Jwt:MinuteExpiration")),
+                SigningCredentials = credentials,
+                Issuer = configuration["Jwt:Issuer"],
+                Audience = configuration["Jwt:Audience"]
+            };
+
+            var handler = new JsonWebTokenHandler();
+            string token = handler.CreateToken(tokenDescriptor);
+
+            return token;
+        }
+
+    }
+}
